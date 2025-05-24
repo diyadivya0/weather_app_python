915c1b0f5cfc74f24dd85aee8b79183b2fdf354b
diyadivya0 <145775281+diyadivya0@users.noreply.github.com>
2025-05-24 18:36:16 +0530

    Add files via upload

---
+++ requirements.txt.py
@@ -0,0 +1,13 @@
+Libraries/Packages need to be installed:-
+
+import asyncio
+import tkinter as tk
+from tkinter import ttk, messagebox
+import requests
+import geocoder
+import platform
+from datetime import datetime, timedelta
+from PIL import Image, ImageTk
+import io
+import base64
+import urllib.request
